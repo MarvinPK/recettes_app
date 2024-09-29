4d4184d51e87ca124f08e2a52e0965d3a563b52d
Prioux-khaye Marvin <marvin.priouxkhaye@hotmail.fr>
2024-09-29 14:32:01 +0200

    list recette par categories

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -5,7 +5,7 @@ import {createDrawerNavigator} from '@react-navigation/drawer'
 import HomeScreen from '../screens/Home/HomeScreen';
 import CategoriesScreen from '../screens/Categories/CategoriesScreen';
 // import RecipeScreen from '../screens/Recipe/RecipeScreen';
-// import RecipesListScreen from '../screens/RecipesList/RecipesListScreen';
+import RecipesListScreen from '../screens/RecipesList/RecipesListScreen';
 import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
 // import IngredientScreen from '../screens/Ingredient/IngredientScreen';
 // import SearchScreen from '../screens/Search/SearchScreen';
@@ -29,7 +29,7 @@ function MainNavigator() {
       <Stack.Screen name='Home' component={HomeScreen} /> 
       <Stack.Screen name='Categories' component={CategoriesScreen}/>
       {/* <Stack.Screen name='Recipe' component={RecipeScreen}/> */}
-      {/* <Stack.Screen name='RecipesList' component={RecipesListScreen} /> */}
+      <Stack.Screen name='RecipesList' component={RecipesListScreen} />
       {/* <Stack.Screen name='Ingredient' component={IngredientScreen} /> */}
       {/* <Stack.Screen name='Search' component={SearchScreen} /> */}
       {/* <Stack.Screen name='IngredientsDetails' component={IngredientsDetailsScreen} /> */}
